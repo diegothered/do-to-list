6e3b75417badf6b83caa901ab4af2e73f245030b
Student <example@example.com>
2019-11-04 16:31:32 -0800

    fix list repeat bug.

--- js/scripts.js
+++ js/scripts.js
@@ -7,44 +7,30 @@ TaskList.prototype.addTask = function (task) {
   this.tasks.unshift(task);
 
 };
-// TaskList.prototype.assignId = function (task) {
-  //   this.currentId += 1;
-  //   return this.currentId;
-  //
-  //   };
+//UI
+var userTaskList = new TaskList ();
 
-  // function Task(task) {
-    //   this.task = task,
-    // };
+$(document).ready(function() {
 
-    //UI
-    var userTaskList = new TaskList ();
-
-    $(document).ready(function() {
-
-      $("#task-input").submit(function(event) {
-        event.preventDefault();
-        // $("#").hide();
-        var task = $("input#task").val();
-        userTaskList.addTask(task);
-        $(':input').val('');
-  $('#task-list').append("<li class='tasks'>" + userTaskList.tasks[0] + "</li>")
-  $(".tasks").click(function() {
-    $(this).addClass('clicked');
-
-      });
-  $(".tasks").dblclick(function() {
-    $(this).toggle('clicked');
-
-      });
-
-      // $("#make-list").click(function() {
-      //   console.log("hi");
-      //   for(i =0; i<userTaskList.tasks.length; i++) {
-      //   $('#task-list').append("<li>" + userTaskList.tasks[i] + "</li>")
-      // };
-      // $('li').click(function() {
-      //   $(this).toggleClass('clicked');
-      // })
-      });
+  $("#task-input").submit(function(event) {
+    event.preventDefault();
+    // $("#").hide();
+    var task = $("input#task").val();
+    userTaskList.addTask(task);
+    $(':input').val('');
+    $('#task-list').text("");
+    // $('#task-list').append("<li id=" + tasks + "</li>" + userTaskList.tasks[i] + "</li>")
+    for(i=0; i<userTaskList.tasks.length; i++) {
+      console.log(userTaskList.tasks[i]);
+      $('#task-list').append("<li id='"+ i + "'" + "</li>" + userTaskList.tasks[i] + "</li>");
+    }
+    $('li').click(function() {
+      $(this).addClass('clicked');
     });
+    $('li').dblclick(function() {
+      $(this).toggle('clicked');
+
+    });
+
+  });
+});
