04a61249a412082516e75e7211014cad03c9fdc2
Student <example@example.com>
2019-11-04 16:01:13 -0800

    add input field that will build a to-do list. User can then single click to strike through or double-click to take off the list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,50 @@
 //Backend
+function TaskList() {
+  this.tasks = []
+}
 
+TaskList.prototype.addTask = function (task) {
+  this.tasks.unshift(task);
 
-//UI
+};
+// TaskList.prototype.assignId = function (task) {
+  //   this.currentId += 1;
+  //   return this.currentId;
+  //
+  //   };
 
-$(document).ready(function() {
+  // function Task(task) {
+    //   this.task = task,
+    // };
 
-  $("#").submit(function(event) {
-    event.preventDefault();
-    $("#").hide();
+    //UI
+    var userTaskList = new TaskList ();
 
-  });
+    $(document).ready(function() {
 
-});
+      $("#task-input").submit(function(event) {
+        event.preventDefault();
+        // $("#").hide();
+        var task = $("input#task").val();
+        userTaskList.addTask(task);
+        $(':input').val('');
+  $('#task-list').append("<li class='tasks'>" + userTaskList.tasks[0] + "</li>")
+  $(".tasks").click(function() {
+    $(this).addClass('clicked');
+
+      });
+  $(".tasks").dblclick(function() {
+    $(this).toggle('clicked');
+
+      });
+
+      // $("#make-list").click(function() {
+      //   console.log("hi");
+      //   for(i =0; i<userTaskList.tasks.length; i++) {
+      //   $('#task-list').append("<li>" + userTaskList.tasks[i] + "</li>")
+      // };
+      // $('li').click(function() {
+      //   $(this).toggleClass('clicked');
+      // })
+      });
+    });
